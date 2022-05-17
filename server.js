e602386bddc55b32f79cc3f0141e4d5f397e0d7b
B.M.U.S.Kularatne_IT19029528 <67848552+upulka@users.noreply.github.com>
2022-05-17 14:17:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 require("dotenv").config();
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8070;
 
 app.use(cors());
 app.use(bodyParser.json());
