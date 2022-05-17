bff86964800a2d37c15e25fba02f8616f5704b1a
B.M.U.S.Kularatne_IT19029528 <67848552+upulka@users.noreply.github.com>
2022-05-17 13:39:02 +0530

    Changes in the port

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 require("dotenv").config();
 
 
-const PORT = process.env.PORT || 8070;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
